8528a0a04453f7925f754e92e9939cff29e8664c
Sakshi Ghanwat <141915486+SakshiGhanwat@users.noreply.github.com>
2026-04-18 16:01:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    if (!str) return null;
+
+    let freq = {};
+
+    // Step 1: count frequency
+    for (let char of str) {
+        freq[char] = (freq[char] || 0) + 1;
+    }
+
+    // Step 2: find first non-repeating
+    for (let char of str) {
+        if (freq[char] === 1) {
+            return char;
+        }
+    }
+
+    return null;
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
